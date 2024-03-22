7dce4658adaf9deee1c55a935490a4c2fdfb0315
hilde-Otieno <meghanadhiambootieno@gmail.com>
2024-03-22 08:40:03 +0300

    add file

---
+++ arrays.js
@@ -0,0 +1,29 @@
+let arr1 = [3,7,34,90,12]
+console.log(arr1.slice(-1))
+
+let arr2 = [true, "green", "where", 12, 56]
+console.log(arr2.slice(-1))
+
+let myPets = ["Cow", "Bird", "Snake", "Dog"]
+let newPets = myPets.join(" ");
+
+const arr3 = [-5, 9, 5, 3, 2, -3, 6, 8, 4, 1];
+arr3.sort()
+console.log(arr3);
+
+var arr = ["boy", "man", "girl", "school", "girl", "woman"];
+function removeDuplicates(arr){
+    return arr.filter(()) => arr.indexOf() === index);}
+    console.log(removeDuplicates(arr))
+
+let arr5 = ["the", "way", "x", 4];
+let substring = "way"
+if(arr5.indexOf(arr5) !==1) {
+    console.log({substring});
+}
+
+let word = "renniw";
+let sortword = (string) =>{
+    return string.split("").sort().join("")
+    console.log(sortword("renniw"));
+}
